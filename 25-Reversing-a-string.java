3aadde44ac812f580d0c545f74399bc5dc5404de
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-09-11 21:02:31 +0530

    Create 25-Reversing-a-string.java

---
+++ 25-Reversing-a-string.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder string = new StringBuilder("Reverse this!");
+    string.reverse();
+    System.out.println("Original String: Reverse this!");
+    System.out.println("Reversed String: " + string);
+  }
+}
