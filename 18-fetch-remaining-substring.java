281d7289183d59db8932209b9b95d01fe9a34e9d
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-09-11 22:05:19 +0530

    Update 18-fetch-remaining-substring.java

--- 18-fetch-remaining-substring.java
+++ 18-fetch-remaining-substring.java
@@ -1,5 +1,7 @@
-public class q17_fetch_remaining_string {
+public class Main {
     public static void main(String[] args){
+        String a = "How and what happened?";
         System.out.println(a.Subsequence(a.indexOf(args[0] + args[0.length] + 1, a.length())));
     }
 }
+// args will be "and"
