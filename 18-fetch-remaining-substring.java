1cf2d5325c96340d26c25b860392d6f35a1e8152
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-09-11 21:56:02 +0530

    Lab Codes

---
+++ 18-fetch-remaining-substring.java
@@ -0,0 +1,5 @@
+public class q17_fetch_remaining_string {
+    public static void main(String[] args){
+        System.out.println(a.Subsequence(a.indexOf(args[0] + args[0.length] + 1, a.length())));
+    }
+}
