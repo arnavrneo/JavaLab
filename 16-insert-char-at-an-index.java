ceebb4c2e9d700a5b4f86803706d38af92c10a5e
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-09-11 21:41:59 +0530

    Java Codes

---
+++ 16-insert-char-at-an-index.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        StringBuilder a = new StringBuilder("Hello Java");
+        a.insert(4,"O");
+        System.out.println(a);
+    }
+}
